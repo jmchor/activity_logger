0df8cd2ee2ad6143d5fe677c86bb880de71345af
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-07 19:16:25 +0100

    Add login GET route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const User = require('../models/User.model')
 const mongoose = require('mongoose')
 
+//password stuff here
+
 //require middleware here
 
 // login GET route here
+
+router.get('/login', (req, res, next) => {
+    res.render('/auth/login')
+})
+
+
 
 
 //login POST route here
