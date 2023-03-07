07565060cf637b3cfd5a7e144fc5af132600d8b1
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-07 19:12:20 +0100

    Add auth.routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User.model')
+const mongoose = require('mongoose')
+
+//require middleware here
+
+// login GET route here
+
+
+//login POST route here
+
+
