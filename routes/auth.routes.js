fe5d3a9a1b3992b288f221aa8006162a8f0df194
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-08 09:32:12 +0100

    Create SignUp GET route, format code with Prettier

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User.model')
-const mongoose = require('mongoose')
+const User = require('../models/User.model');
+const mongoose = require('mongoose');
 
 //password stuff here
 
@@ -9,37 +9,46 @@ const mongoose = require('mongoose')
 
 //user screen (you get here when calling '/user')
 
-router.get('/', async (req, res, next ) => {
-    res.render('index-logged-out')
-})
+router.get('/', async (req, res, next) => {
+	res.render('index-logged-out');
+});
 
 // login GET route here
 
 router.get('/login', (req, res, next) => {
-    res.render('auth/login')
-})
-
+	res.render('auth/login');
+});
 
 //login POST route here
 
+router.post('/login', async (req, res, next) => {
+	const { username, password, email } = req.body;
 
-router.post('/login', async (req,res,next) => {
-    const { username, password, email} = req.body
+	if (username === '' || password === '') {
+		res.render('auth/login', {
+			errorMessage: 'Please enter both email and password to log in.',
+		});
+		return;
+	}
 
-    if (username === '' || password === '') {
-        res.render('auth/login', {
-                errorMessage: 'Please enter both email and password to log in.',
-        });
-        return;
-}
-
-try {
-        const user = await User.findOne({ username });
-        res.redirect('/index-logged-in')
-
-} catch (error) {
-        next(error);
-}
+	try {
+		const user = await User.findOne({ username });
+		res.redirect('/index-logged-in');
+	} catch (error) {
+		next(error);
+	}
 });
+
+// signup GET route
+
+router.get('/signup', async (req, res, next) => {
+
+        res.render('auth/sign-up')
+
+})
+
+
+
+// signup POST route
 
 module.exports = router;
