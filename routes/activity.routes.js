ff5d038ae20af1fc24dce10a0d1d558bc0bdc1a4
Lukas Merlin Jürgens <108548830+lukasmerlin@users.noreply.github.com>
2023-03-09 21:08:14 +0100

    Update activity.routes.js

--- routes/activity.routes.js
+++ routes/activity.routes.js
@@ -3,8 +3,31 @@ const router = express.Router();
 const mongoose = require('mongoose');
 // Import the middleware for authentication here
 const { isLoggedIn } = require('../middleware/routeguard');
+const Activity = require('../models/activity');
 
+router.get('/activities/new', (req, res) => {
+  res.render('create-activity');
+});
 
+router.post('/activities', isLoggedIn, async (req, res) => {
+  const { title, description, daysOfWeek, repeat, specificDate } = req.body;
 
+  const activity = new Activity({
+    userId: req.user._id, // assuming there is an authenticated user
+    title,
+    description,
+    daysOfWeek,
+    repeat,
+    specificDate,
+  });
+
+  try {const savedActivity = await activity.save();
+    res.redirect('/activities/' + savedActivity._id);
+  } catch (error) {
+    console.log(error);
+    res.render('create-activity', { errorMessage: 'Error creating activity.' });
+  }
+});
+  
 
 module.exports = router;
