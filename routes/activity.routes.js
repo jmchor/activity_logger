b2b7076aa0ab68e2435324b4703822a68efb9653
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-09 21:26:58 +0100

    renamed Model path,  routes & redirect to home

--- routes/activity.routes.js
+++ routes/activity.routes.js
@@ -3,31 +3,33 @@ const router = express.Router();
 const mongoose = require('mongoose');
 // Import the middleware for authentication here
 const { isLoggedIn } = require('../middleware/routeguard');
-const Activity = require('../models/activity');
+const Activity = require('../models/Activity.model')
 
-router.get('/activities/new', (req, res) => {
-  res.render('create-activity');
+router.get('/create', (req, res) => {
+	res.render('create-activity');
 });
 
-router.post('/activities', isLoggedIn, async (req, res) => {
-  const { title, description, daysOfWeek, repeat, specificDate } = req.body;
+router.post('/create', isLoggedIn, async (req, res) => {
+	const { title, description, daysOfWeek, repeat, specificDate } = req.body;
 
-  const activity = new Activity({
-    userId: req.user._id, // assuming there is an authenticated user
-    title,
-    description,
-    daysOfWeek,
-    repeat,
-    specificDate,
-  });
+	const activity = new Activity({
+		userId: req.session.currentUser._id, // assuming there is an authenticated user
+		title,
+		description,
+		daysOfWeek,
+		repeat,
+		specificDate,
+	});
 
-  try {const savedActivity = await activity.save();
-    res.redirect('/activities/' + savedActivity._id);
-  } catch (error) {
-    console.log(error);
-    res.render('create-activity', { errorMessage: 'Error creating activity.' });
-  }
+	try {
+        //TODO write hbs view to see all views or weekly view
+		// const savedActivity = await activity.save();
+		// res.redirect('/create/' + savedActivity._id);
+        res.redirect('/home')
+	} catch (error) {
+		console.log(error);
+		res.render('create-activity', { errorMessage: 'Error creating activity.' });
+	}
 });
-  
 
 module.exports = router;
