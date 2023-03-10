f80e25d217ba86b7db0f9886d9822e995cfe9d34
Nana Chuto <mail@nanachuto.com>
2023-03-10 18:44:00 +0700

    Removed loggedout-layout.hbs, testing routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-	res.render('index', { layout: 'loggedout-layout' });
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 module.exports = router;
