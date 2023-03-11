7bec4d89ea63387cb4a99ee7d49e57f7e1f7f0c2
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-11 13:12:51 +0100

    Add functions to show/hide weekdays in create page

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("activity_logger JS imported successfully!");
 });
+
+const repeat = document.getElementById('repeat');
+const days = document.getElementById('days');
+const specificDay = document.getElementById('specific-day');
+
+function selectRecurrence() {
+
+
+  if (repeat.value === 'weekly') {
+    days.style.display = 'block';
+    specificDay.style.display = 'none';
+
+  } else if (repeat.value === 'once') {
+    specificDay.style.display = 'block';
+    days.style.display = 'none';
+  }
+
+}
+//
+window.onload = function () {
+  specificDay.style.display = 'none';
+  days.style.display = 'none';
+
+}
