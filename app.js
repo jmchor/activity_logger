4805ea7431f9d9846dea29b816bc210db6ca3f65
Johannes Chorzempa <jchorzempa@posteo.org>
2023-03-17 20:37:50 +0100

    Edit logo and app name

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ require("./config/session.config")(app);
 
 // default value for title local
 const capitalize = require("./utils/capitalize");
-const projectName = "TaskTastrophy";
+const projectName = "TaskTastrophe";
 
 app.locals.appTitle = `${capitalize(projectName)} created by JLN with IronLauncher`;
 
